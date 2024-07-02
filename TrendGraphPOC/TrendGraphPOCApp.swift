7c59ac09702f4c87bd19600b23ef7b77f2a87c03
NadaKamalElmasry <ne0423@dexcom.com>
2024-07-02 15:43:06 +0300

    Initial Commit

---
+++ TrendGraphPOC/TrendGraphPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendGraphPOCApp.swift
+//  TrendGraphPOC
+//
+//  Created by Nada Elmasry on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrendGraphPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
